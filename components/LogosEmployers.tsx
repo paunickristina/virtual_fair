702f218a81d5149cf83e3914fa934e17812c667f
paunickristina <paunickristina@gmail.com>
2023-03-02 15:16:43 +0100

    hp block with horizontal scroll

--- components/LogosEmployers.tsx
+++ components/LogosEmployers.tsx
@@ -4,36 +4,71 @@ import Image from "next/image";
 
 // images
 import microsoft from "../public/assets/logos/microsoft.png";
-import nestle from "../public/assets/logos/nestle.png";
 
 const LogosEmployers = (): JSX.Element => {
   return (
-    <section className="wrapper lg:wrapper mb-28">
-      check this: scroll and border width?
-      <div className="l flex items-center border-y border-lightGray-50">
-        <div className="py-10 pl-8 pr-12 border-r border-lightGray-50">
-          <span className="tag tag-big bg-lightGray-100">ПОСЛОДАВЦИ</span>
+    <>
+      <section className="wrapper lg:wrapper mb-28">
+        <div className="l flex items-center h-[105px] border-y border-lightGray-50">
+          <div className="py-8 pl-8 pr-[53px] border-r border-lightGray-50">
+            <span className="tag tag-big bg-lightGray-100">ПОСЛОДАВЦИ</span>
+          </div>
+          <div className="w-[105px] lg:h-[696px] xl:h-[1111px] overflow-y-auto overflow-x-hidden no-scrollbar -rotate-90 origin-top-right translate-y-[50%] relative bottom-1/2 -translate-x-[105px] pt-48">
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+            <p className="w-[130px] h-52 rotate-90 origin-top-right relative left-[-54%]">
+              <Link href="">
+                <Image alt="" src={microsoft} className="w-full mx-4" />
+              </Link>
+            </p>
+          </div>
         </div>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={microsoft} />
-        </Link>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={nestle} />
-        </Link>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={microsoft} />
-        </Link>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={nestle} />
-        </Link>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={microsoft} />
-        </Link>
-        <Link href="" className="w-32 mx-16">
-          <Image alt="image" src={nestle} />
-        </Link>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
