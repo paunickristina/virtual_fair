80f323834ebc785dca13944ea79720c331dadb5e
paunickristina <paunickristina@gmail.com>
2023-03-08 11:16:40 +0100

    signup confirm

--- components/signup/Signup.tsx
+++ components/signup/Signup.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 // components
 import SignupForm from "./SignupForm";
+import SignupConfirm from "./SignupConfirm";
 
 // images
 import logo from "@/public/assets/logo.png";
@@ -11,7 +12,7 @@ import logo from "@/public/assets/logo.png";
 const Signup = (): JSX.Element => {
   return (
     <section className="flex items-center min-h-screen">
-      <div className="w-[34.4%] mb-[14px]">
+      <div className="w-[34.4%] mb-[14px] h-[657px]">
         <div className="w-[80.7%] ml-[8.9%] ">
           <div className="mb-[61px]">
             <Link href="/" className="">
@@ -19,6 +20,7 @@ const Signup = (): JSX.Element => {
             </Link>
           </div>
           <SignupForm />
+          {/* <SignupConfirm /> */}
         </div>
       </div>
       <div
---
+++ components/signup/SignupConfirm.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+
+const SignupConfirm: FunctionComponent = (): JSX.Element => {
+  return (
+    <div className="pt-[152px]">
+      <p className="text-[24px] leading-[29px] px-[26px] py-9 border border-lightGray-400 rounded-lg">
+        Молим Вас проверите ваше електронско поштанско сандуче или проверите
+        спам
+      </p>
+      <div className="flex justify-end pt-[185px]">
+        <button className="btn btn-red px-[60px]">Даље</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignupConfirm;
