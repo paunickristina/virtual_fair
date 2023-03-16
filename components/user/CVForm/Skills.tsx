ee1169260131861b05739831ddbf499e5fa17020
paunickristina <paunickristina@gmail.com>
2023-03-16 11:08:03 +0100

    cv form - append new block

--- components/user/CVForm/Skills.tsx
+++ components/user/CVForm/Skills.tsx
@@ -1,27 +1,42 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 type IProps = {
-  // src: string;
   // className: string;
 };
 
-const Skills: FunctionComponent<IProps> = (
-  {
-    // src,
-    // className,
-  }
-): JSX.Element => {
-  return (
-    <div className="bg-white bordered px-5 pt-10 mb-[30px]">
-      <h3 className="text-2xl font-normal mb-[34px]">Вештине</h3>
+const Skills: FunctionComponent<IProps> = (): JSX.Element => {
+  const [numChildren, setNumChildren] = useState<number>(1);
+  const children = [];
+
+  const ChildComponent = () => {
+    return (
       <div className="flex flex-wrap justify-between mb-[18px]">
         <label className="w-full">
           <span className="signup-span">Вештина</span>
           <input className="user-cv-input" type="text" />
         </label>
       </div>
+    );
+  };
+
+  for (let i = 0; i < numChildren; i++) {
+    children.push(<ChildComponent key={i} />);
+  }
+
+  const handleClick = () => {
+    setNumChildren((count) => count + 1);
+  };
+
+  return (
+    <div className="bg-white bordered px-5 pt-10 mb-[30px]">
+      <h3 className="text-2xl font-normal mb-[34px]">Вештине</h3>
+      {children}
       <div className="flex justify-end mb-[34px]">
-        <button className="btn btn-white w-[120px] px-0 py-[7px] border border-primary-700">
+        <button
+          type="button"
+          onClick={handleClick}
+          className="btn btn-white w-[120px] px-0 py-[7px] border border-primary-700"
+        >
           + Додај
         </button>
       </div>
